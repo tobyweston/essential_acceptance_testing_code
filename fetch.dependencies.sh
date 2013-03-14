be04be4706c8f65b7cf9a8fc55e90435825e9af5
tobyweston <toby.weston@gmail.com>
2013-03-14 14:06:29 +0000

    adding wiremock stub for the implementation of FakeYahoo.

---
+++ fetch.dependencies.sh
@@ -0,0 +1,1 @@
+java -Dhttp.proxyHost=hk-proxy.ap.hedani.net -Dhttp.proxyPort=8080 -jar shavenmaven-61.jar build.dependencies lib/wiremock
--- test/fakes/com/example/stocks/core/FakeYahoo.java
+++ test/fakes/com/example/stocks/core/FakeYahoo.java
@@ -1,18 +1,36 @@
 package com.example.stocks.core;
 
 import com.example.stocks.infrastructure.server.Server;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.UrlMatchingStrategy;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class FakeYahoo implements Server {
 
     public static final Integer port = 8001;
 
+    private final WireMockServer server = new WireMockServer(new WireMockConfiguration().port(port));
+
     @Override
     public void start() {
+        server.start();
+    }
 
+    public void stub(String url, String response) {
+        stubFor(get(urlStartsWith("/v1/public/yql")).willReturn(aResponse().withBody("370")));
     }
 
     @Override
     public void stop() {
-
+        server.stop();
     }
+
+    public static UrlMatchingStrategy urlStartsWith(String url) {
+        UrlMatchingStrategy urlStrategy = new UrlMatchingStrategy();
+        urlStrategy.setUrlPattern(url + ".*");
+        return urlStrategy;
+    }
+
 }
