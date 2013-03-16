3788630301af3c7dacb5548948dfae8a5333839f
Toby Weston <toby.weston@gmail.com>
2013-03-16 12:31:35 +0000

    added concordion "system" test

---
+++ test/acceptance/java/com/example/stocks/infrastructure/rest/PortfolioAcceptanceTest.java
@@ -0,0 +1,11 @@
+package com.example.stocks.infrastructure.rest;
+
+import org.concordion.api.ExpectedToFail;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+@ExpectedToFail
+public class PortfolioAcceptanceTest {
+
+}
---
+++ test/acceptance/resources/specifications.js
@@ -0,0 +1,4 @@
+
+function toggleDetails() {
+    $('.details').toggle('fast');
+}
