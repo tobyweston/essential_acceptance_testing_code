c19e47e23f6cbf7cafee856fabf79c834d39aa8b
tobyweston <toby.weston@gmail.com>
2013-03-18 14:32:12 +0000

    sketching the ui transport acceptance test

---
+++ test/acceptance/java/com/example/stocks/portfolio/PortfolioUiTransportTest.java
@@ -0,0 +1,9 @@
+package com.example.stocks.portfolio;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class PortfolioUiTransportTest {
+
+}
