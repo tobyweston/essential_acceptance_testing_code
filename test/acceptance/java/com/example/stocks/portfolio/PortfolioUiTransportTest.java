f6a3e8b55a0545a84ecb718b9e50c68dd8095e5a
Toby Weston <toby.weston@gmail.com>
2013-03-25 13:23:06 +0000

    tweaking test

--- test/acceptance/java/com/example/stocks/portfolio/PortfolioUiTransportTest.java
+++ test/acceptance/java/com/example/stocks/portfolio/PortfolioUiTransportTest.java
@@ -1,10 +1,13 @@
 package com.example.stocks.portfolio;
 
+import com.example.stocks.CopyResourcesToOutputFolder;
 import com.example.stocks.driver.pages.LandingPage;
 import com.example.stocks.infrastructure.HttpServer;
 import com.example.stocks.infrastructure.client.WebUi;
 import com.github.tomakehurst.wiremock.client.VerificationException;
 import org.concordion.api.ExpectedToPass;
+import org.concordion.api.extension.ConcordionExtension;
+import org.concordion.api.extension.Extension;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.After;
 import org.junit.Before;
@@ -13,16 +16,18 @@ import org.junit.runner.RunWith;
 import java.net.MalformedURLException;
 
 import static com.example.stocks.infrastructure.UrlMatchingStrategies.urlEndingWith;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 
 @RunWith(ConcordionRunner.class)
 @ExpectedToPass
 public class PortfolioUiTransportTest {
 
+    @Extension public ConcordionExtension extension = new CopyResourcesToOutputFolder(this.getClass());
+
     private final HttpServer client = new WebUi();
     private final FakeHttpServer application = new FakeHttpServer(8000);
     private final LandingPage ui = new LandingPage();
-    private final String expectedUrl = "/portfolio/0001";
+
+    private final static String expectedUrl = "/portfolio/0001";
 
     @Before
     public void start() {
@@ -31,7 +36,7 @@ public class PortfolioUiTransportTest {
     }
 
     public String requestPortfolioValue() throws MalformedURLException {
-        application.stub(urlEndingWith(expectedUrl), aResponse().withBody("1000"));
+//        application.stub(urlEndingWith(expectedUrl), aResponse().withBody("1000"));
         ui.navigateToLandingPage().requestValuationForShares(100);
         return expectedUrl;
     }
