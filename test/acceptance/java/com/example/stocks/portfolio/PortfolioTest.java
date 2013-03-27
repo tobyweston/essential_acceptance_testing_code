142757fe74b228002a574b80c7fa6544252c0878
tobyweston <toby.weston@gmail.com>
2013-03-27 15:58:42 +0000

    added breadcrumbs

---
+++ test/acceptance/java/com/example/stocks/portfolio/PortfolioTest.java
@@ -0,0 +1,11 @@
+package com.example.stocks.portfolio;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class PortfolioTest {
+
+}
