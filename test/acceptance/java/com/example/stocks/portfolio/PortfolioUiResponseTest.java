1c3e14024d9221f74b5e9ff3ad3ee1b1d3699489
tobyweston <toby.weston@gmail.com>
2013-03-28 15:39:00 +0000

    asserting the response returned by the server is actually displayed correctly.

--- test/acceptance/java/com/example/stocks/portfolio/PortfolioUiResponseTest.java
+++ test/acceptance/java/com/example/stocks/portfolio/PortfolioUiResponseTest.java
@@ -33,6 +33,7 @@ public class PortfolioUiResponseTest {
         application.start();
     }
 
+    // TODO don't input this from the test
     public void requestPortfolioValue(String headerName, String headerValue, String body) throws MalformedURLException {
         application.stub(urlEndingWith("/portfolio/0001"), aResponse().withHeader(headerName, headerValue).withBody(body));
         ui.navigateToLandingPage().requestValuationForShares(100);
