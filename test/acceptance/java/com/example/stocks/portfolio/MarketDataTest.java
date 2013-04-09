788dd37c859d15c4571b2aaf6ff280283a8efe99
tobyweston <toby.weston@gmail.com>
2013-04-09 10:07:20 +0100

    modified test to use a book with two stocks

--- test/acceptance/java/com/example/stocks/portfolio/MarketDataTest.java
+++ test/acceptance/java/com/example/stocks/portfolio/MarketDataTest.java
@@ -6,6 +6,7 @@ import com.example.stocks.core.Money;
 import com.example.stocks.core.Portfolio;
 import com.example.stocks.core.StubBook;
 import org.concordion.api.ExpectedToPass;
+import org.concordion.api.MultiValueResult;
 import org.concordion.api.extension.ConcordionExtension;
 import org.concordion.api.extension.Extension;
 import org.concordion.integration.junit4.ConcordionRunner;
@@ -28,11 +29,12 @@ public class MarketDataTest {
     private final MarketData marketData = context.mock(MarketData.class);
     private final Portfolio portfolio = new Portfolio(new StubBook().add(Amazon), marketData);
 
-    public String verifySymbolCheckedWas(final String symbol) {
+    public MultiValueResult verifySymbolCheckedWas(final String firstSymbol, final String secondSymbol) {
         context.checking(new Expectations() {{
-            oneOf(marketData).getPrice(fromSymbol(symbol)); will(returnValue(new Money(100)));
+            oneOf(marketData).getPrice(fromSymbol(firstSymbol)); will(returnValue(new Money(100)));
+            oneOf(marketData).getPrice(fromSymbol(secondSymbol)); will(returnValue(new Money(200)));
         }});
         portfolio.value();
-        return symbol;
+        return MultiValueResult.multiValueResult().with("firstSymbol", firstSymbol).with("secondSymbol", secondSymbol);
     }
 }
