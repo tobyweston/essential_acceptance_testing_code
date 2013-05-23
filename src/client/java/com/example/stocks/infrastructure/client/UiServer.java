5b0660b370d7b1bacffa9986726f20f5c71b4874
tobyweston <toby.weston@gmail.com>
2013-05-23 15:08:55 +0100

    trying to get UtterlyIdle to serve web fonts

--- src/client/java/com/example/stocks/infrastructure/client/UiServer.java
+++ src/client/java/com/example/stocks/infrastructure/client/UiServer.java
@@ -4,6 +4,7 @@ import com.example.stocks.infrastructure.Defect;
 import com.example.stocks.infrastructure.HttpServer;
 import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.ServerConfiguration;
+import com.googlecode.utterlyidle.dsl.StaticBindingBuilder;
 import com.googlecode.utterlyidle.httpserver.RestServer;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import static com.example.stocks.infrastructure.rest.URLs.defaultPackageUrl;
 import static com.googlecode.totallylazy.Strings.EMPTY;
 import static com.googlecode.utterlyidle.ApplicationBuilder.application;
 import static com.googlecode.utterlyidle.ServerConfiguration.defaultConfiguration;
+import static com.googlecode.utterlyidle.dsl.DslBindings.bindings;
 
 public class UiServer implements HttpServer {
 
@@ -22,7 +24,12 @@ public class UiServer implements HttpServer {
     @Override
     public void start() {
         try {
-            Application application = application().content(defaultPackageUrl(getClass()), EMPTY).build();
+            StaticBindingBuilder extensions = new StaticBindingBuilder(defaultPackageUrl(getClass()))
+                .set("otf", "application/octet-stream")
+                .set("eot", "application/octet-stream")
+                .set("ttf", "application/octet-stream")
+                .set("woff", "application/octet-stream");
+            Application application = application().content(defaultPackageUrl(getClass()), EMPTY).add(bindings(extensions)).build();
             ServerConfiguration configuration = defaultConfiguration().port(port);
             server = new RestServer(application, configuration);
         } catch (Exception e) {
